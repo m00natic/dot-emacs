bd8f8d61f65d7c018ef38688ba0fc82051ebc881
Andrey Kotlarski <akotlarski@vayant.com>
2012-08-29 14:30:20 +0300

    Refactor automatic switching of themes on sunset/sunrise.

--- conf/my-themes.el
+++ conf/my-themes.el
@@ -20,17 +20,15 @@
 	(and (load-theme 'andr t t)
 	     (load-theme 'andr-dark t t)))
 
-  (when +old-emacs+
-    (disable-theme 'andr)
-    (disable-theme 'andr-dark))
-
   (defmacro switch-faces (&optional light)
     "Set dark faces.  With prefix, LIGHT."
     (if light '(enable-theme 'andr)
       '(enable-theme 'andr-dark)))
 
   ;; automatically switch faces on sunrise and sunset
-  (when (require 'solar nil t)
+  (when (and (require 'solar nil t)
+	     calendar-latitude calendar-longitude calendar-time-zone)
+
     (defun solar-time-to-24 (time-str)
       "Convert solar type string TIME-STR to 24 hour format."
       (if (string-match "\\(.*\\)[/:-]\\(..\\)\\(.\\)" time-str)
@@ -42,48 +40,54 @@
 		  (match-string 2 time-str))
 	time-str))
 
-    (defun my-colours-set ()
-      "Set colours of new FRAME according to time of day.
-Set timer that runs on next sunset or sunrise, whichever sooner."
-      (if (and calendar-latitude calendar-longitude
-	       calendar-time-zone)
-	  (let ((solar-info (solar-sunrise-sunset
-			     (calendar-current-date))))
-	    (let ((sunrise-string (solar-time-string
-				   (caar solar-info)
-				   (car (cdar solar-info))))
-		  (sunset-string (solar-time-string
-				  (car (cadr solar-info))
-				  (cadr (cadr solar-info))))
-		  (current-time-string (format-time-string "%H:%M")))
-	      (cond
-	       ((string-lessp current-time-string ; before dawn
-			      (solar-time-to-24 sunrise-string))
-		(switch-faces)
-		(run-at-time sunrise-string nil 'my-colours-set))
-	       ((not (string-lessp current-time-string ; evening
-				   (solar-time-to-24 sunset-string)))
-		(switch-faces)
-		(run-at-time
-		 (let ((tomorrow (calendar-current-date 1)))
-		   (let* ((next-solar-rise (car (solar-sunrise-sunset
-						 tomorrow)))
-			  (next-rise (solar-time-to-24
-				      (solar-time-string
-				       (car next-solar-rise)
-				       (cadr next-solar-rise)))))
-		     (encode-time 0 (string-to-number
-				     (substring next-rise 3 5))
-				  (string-to-number
-				   (substring next-rise 0 2))
-				  (cadr tomorrow) (car tomorrow)
-				  (car (cddr tomorrow)))))
-		 nil 'my-colours-set))
-	       (t (switch-faces t)	; daytime
-		  (run-at-time sunset-string nil 'my-colours-set)))))
-	(switch-faces t)))
+    (defun my-colours-switch (event)
+      "Switch colours according to sun EVENT.
+Set timer to do another switch at the next sun event."
+      (if (eq event 'sunrise)
+	  (progn (switch-faces t)
+		 (let* ((solar-info (solar-sunrise-sunset
+				     (calendar-current-date)))
+			(sunset-string (solar-time-string
+					(car (cadr solar-info))
+					(cadr (cadr solar-info)))))
+		   (run-at-time sunset-string nil
+				'my-colours-switch 'sunset)))
+	(switch-faces)
+	(let* ((tomorrow (calendar-current-date 1))
+	       (solar-rise (car (solar-sunrise-sunset tomorrow)))
+	       (sunrise (solar-time-to-24 (solar-time-string
+					   (car solar-rise)
+					   (cadr solar-rise)))))
+	  (run-at-time (encode-time 0 (string-to-number
+				       (substring sunrise 3 5))
+				    (string-to-number
+				     (substring sunrise 0 2))
+				    (cadr tomorrow) (car tomorrow)
+				    (car (cddr tomorrow)))
+		       nil 'my-colours-switch 'sunrise))))
 
-    (my-colours-set)))
+    ;; set colours according to time of day
+    ;; and timer that runs on next sunset or sunrise
+    (let ((solar-info (solar-sunrise-sunset
+		       (calendar-current-date))))
+      (let ((sunrise-string (solar-time-string
+			     (caar solar-info)
+			     (car (cdar solar-info))))
+	    (sunset-string (solar-time-string
+			    (car (cadr solar-info))
+			    (cadr (cadr solar-info))))
+	    (current-time-string (format-time-string "%H:%M")))
+	(cond ((string-lessp current-time-string ; before dawn
+			     (solar-time-to-24 sunrise-string))
+	       (switch-faces)
+	       (run-at-time sunset-string nil
+			    'my-colours-switch 'sunrise))
+	      ((string-lessp current-time-string ; daytime
+			     (solar-time-to-24 sunset-string))
+	       (switch-faces t)
+	       (run-at-time sunset-string nil
+			    'my-colours-switch 'sunset))
+	      (t (my-colours-switch 'sunset))))))) ; evening
 
 (provide 'my-themes)
 
