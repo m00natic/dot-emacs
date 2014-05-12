4f6d7a4aa5e6c345203795323a1e86890789668e
Andrey Kotlarski <m00naticus@gmail.com>
2014-05-12 15:25:29 +0300

    Update ErgoEmacs settings to work with latest versions.

--- conf/my-ergo.el
+++ conf/my-ergo.el
@@ -10,7 +10,7 @@
 ;;; ErgoEmacs minor mode
 (custom-set-variables
  '(ergoemacs-keyboard-layout "colemak")
- '(ergoemacs-theme "lvl3"))
+ '(ergoemacs-theme "standard"))
 
 (when (require 'ergoemacs-mode nil t)
   (defun ergoemacs-change-keyboard (layout)
@@ -38,52 +38,44 @@
 
   (when-library
    nil paredit
-   (add-to-list
-    'ergoemacs-minor-mode-layout-lvl3
-    '(activate-lisp-minor-modes-hook
-      (("M-r")
-       ("M-s")
-       ("M-S")
-       ("M-J")
-       ("M-;")
-       ("<f9>" paredit-split-sexp)
-       ("<f10>" paredit-raise-sexp)
-       ("S-<f9>" paredit-join-sexps)
-       ("S-<f10>" paredit-splice-sexp)
-       (kill-visual-line paredit-kill)))))
+   (add-hook 'activate-lisp-minor-modes-hook
+	     (lambda () (ergoemacs-define-overrides
+		    (define-keys paredit-mode-map
+		      "\M-d" nil "\M-r" nil
+		      "\M-s" nil "\M-S" nil
+		      "\M-J" nil "\M-;" nil
+		      (kbd "<f9>") 'paredit-split-sexp
+		      (kbd "<f10>") 'paredit-raise-sexp
+		      (kbd "S-<f9>") 'paredit-join-sexps
+		      (kbd "S-<f10>") 'paredit-splice-sexp
+		      [remap kill-visual-line] 'paredit-kill)))))
 
   (when-library
    nil slime
-   (add-to-list
-    'ergoemacs-minor-mode-layout-lvl3
-    '(slime-connected-hook
-      (("<f11>" slime-next-note)
-       ("<f12>" slime-previous-note))))
+   (add-hook 'slime-connected-hook
+	     (lambda () (ergoemacs-define-overrides
+		    (define-keys slime-mode-map
+		      (kbd "<f11>") 'slime-next-note
+		      (kbd "<f12>") 'slime-previous-note))))
 
-   (add-to-list
-    'ergoemacs-minor-mode-layout-lvl3
-    '(slime-repl-mode-hook
-      (("M-r")
-       ("M-s")
-       ("<f11>" slime-repl-previous-input)
-       ("<f12>" slime-repl-next-input)
-       ("S-<f11>" slime-repl-previous-matching-input)
-       ("S-<f12>" slime-repl-next-matching-input)))))
+   (add-hook 'slime-connected-hook
+	     (lambda ()
+	       (ergoemacs-define-overrides
+		(define-keys slime-repl-mode-map
+		  "\M-r" nil "\M-s" nil
+		  (kbd "<f11>") 'slime-repl-previous-input
+		  (kbd "<f12>") 'slime-repl-next-input
+		  (kbd "S-<f11>")
+		  'slime-repl-previous-matching-input
+		  (kbd "S-<f12>")
+		  'slime-repl-next-matching-input)))))
 
   (setq ergoemacs-handle-ctl-c-or-ctl-x 'only-C-c-and-C-x
 	ergoemacs-use-ergoemacs-metaleft nil
 	ergoemacs-use-ergoemacs-metaright nil)
 
-  (delete "C-d" ergoemacs-redundant-keys)
-  (delete "C-j" ergoemacs-redundant-keys)
-  (delete "C-t" ergoemacs-redundant-keys)
-
-  (ergoemacs-fixed-key "C-f" 'search-forward-regexp)
-  (ergoemacs-fixed-key "C-S-f" 'search-backward-regexp)
-  (ergoemacs-replace-key 'backward-paragraph "C-M-u")
-
-  (ergoemacs-fixed-key "<M-up>" nil)
-  (ergoemacs-fixed-key "<M-down>" nil)
+  (ergoemacs-key "\C-f" 'search-forward-regexp)
+  (ergoemacs-key (kbd "C-S-f") 'search-backward-regexp)
 
   ;; activate
   (ergoemacs-mode 1))
